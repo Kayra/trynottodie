7fa253ef001cae770efbdfbaeb5a331ad6fd1c53
Robby <robby.cowell@gmail.com>
2014-03-08 16:35:38 +0000

    updated food hygiene script

--- data-create/food-hygeine.js
+++ data-create/food-hygeine.js
@@ -13,8 +13,17 @@ function extract (var data) {
 
 	for (var i =0; i < length; i ++) {
 
-		rating = json["EstablishmentCollection"].RatingValue;
-		lat = "";
-		lon ="";
+		rating = json.EstablishmentCollection[i].RatingValue;
+		lat = json.EstablishmentCollection[i].["Geocode"].["Latitude"];
+		lon = json.EstablishmentCollection[i].["Geocode"].["Longitude"];
+
+		var jsonItem = new Object();
+		
+		jsonItem.id = i;
+		jsonItem.type = type;
+		jsonItem.mainS = mainS;
+		jsonItem.rating = rating;
+		jsonItem.lat = lat;
+		jsonItem.lon = lon;
 	}
 }
