7ffe9f8502ad63ce0ec8f4bf8c323652d2ad4ce6
Robby <robby.cowell@gmail.com>
2014-03-09 15:13:22 +0000

    it's kind of working

--- scripts/map.js
+++ scripts/map.js
@@ -12,49 +12,69 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
     var script = document.createElement('script');
-        script.src = "https://gist.githubusercontent.com/RobbyCowell/9446741/raw/aacf08130423752693cccaca62f3f91df0513da1/food-to-map.js";
+    script.src = "https://gist.githubusercontent.com/RobbyCowell/9446741/raw/aacf08130423752693cccaca62f3f91df0513da1/food-to-map.js";
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(script, s);     
 
     script.onload = function(){
 
-        execute(data); 
+        execute(data);
     }
 }
 
 function execute(results) {
 
-    var heatmapData = [];
-
     results = results[0].features;
 
-    console.log(results[0].geometry);
-
-    for (var i = 0; i < results.length; i++) {
+    for (var i = 0; i < results.length; i++) { 
 
         var thisResult = results[i];
 
-
-        if(thisResult != undefined && thisResult.geometry !== undefined && thisResult.geometry.coordinates != undefined){
+        if (thisResult != undefined && thisResult.geometry !== undefined && thisResult.geometry.coordinates != undefined) {
 
             console.log(thisResult);
 
             var coords = thisResult.geometry.coordinates;
             var latLng = new google.maps.LatLng(coords[0], coords[1]);
-            var rating = thisResult.properties.rating;
-            var weightedLoc = {
-                location: latLng,
-                weight: Math.pow(2, rating)
-            };
-            heatmapData.push(weightedLoc);
-
-        }
-
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            });
+         }
     }
-
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData,
-        dissipating: false,
-        map: map
-    });
 }
+
+// function execute(results) {
+
+//     var heatmapData = [];
+
+//     results = results[0].features;
+
+//     console.log(results[0].geometry);
+
+//     for (var i = 0; i < results.length; i++) {
+
+//         var thisResult = results[i];
+
+//         if(thisResult != undefined && thisResult.geometry !== undefined && thisResult.geometry.coordinates != undefined){
+
+//             console.log(thisResult);
+
+//             var coords = thisResult.geometry.coordinates;
+//             var latLng = new google.maps.LatLng(coords[0], coords[1]);
+//             var rating = thisResult.properties.rating;
+//             var weightedLoc = {
+//                 location: latLng,
+//                 weight: Math.pow(2, rating)
+//             };
+//             heatmapData.push(weightedLoc);
+//         }
+
+//     }
+
+//     var heatmap = new google.maps.visualization.HeatmapLayer({
+//         data: heatmapData,
+//         dissipating: false,
+//         map: map
+//     });
+// }
