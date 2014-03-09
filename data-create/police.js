4b8936c622d96705d2dea77953c654239e171a6a
Kayra <KayraAlat@gmail.com>
2014-03-09 13:56:10 +0000

    Honestly I have no idea anymore

--- data-create/police.js
+++ data-create/police.js
@@ -1,9 +1,10 @@
 (function(){
 
 	var spt = document.createElement('script');
-		spt.src = 'https://gist.githubusercontent.com/RobbyCowell/9435502/raw/ab5d00330c432177325f52ad1ca604809875ddb0/food.js';
+		spt.src = 'https://gist.githubusercontent.com/Kayra/9447881/raw/01a8808563e9f69745778a39b149e01dfbeb93cb/police-data.json';
 		spt.onload = function(){
-			console.log(sean);
+			alert("Hit");
+			console.log(daPolice);
 		};
 
 	document.body.appendChild(spt);
@@ -26,27 +27,27 @@ function extract (data) {
 
 	for (var i =0; i < length; i ++) {
 
-		if (json[i].["Crime type"].indexOf("Other theft") != -1) {
+		if (json[i]["Crime type"].indexOf("Other theft") != -1) {
 			rating = 1;
 		}
-		else if (json[i].["Crime type"].indexOf("Anti-social behaviour") != -1  || json[i].["Crime type"].indexOf("Shoplifting") != -1) {
+		else if (json[i]["Crime type"].indexOf("Anti-social behaviour") != -1  || json[i]["Crime type"].indexOf("Shoplifting") != -1) {
 			rating = 2;
 		}
-		else if (json[i].["Crime type"].indexOf("Burglary") != -1) {
+		else if (json[i]["Crime type"].indexOf("Burglary") != -1) {
 			rating = 3;
 		}
-		else if (json[i].["Crime type"].indexOf("Vehicle crime") != -1 || json[i].["Crime type"].indexOf("Criminal damage and arson") != -1 || json[i].["Crime type"].indexOf("Violent crime") != -1) {
+		else if (json[i]["Crime type"].indexOf("Vehicle crime") != -1 || json[i]["Crime type"].indexOf("Criminal damage and arson") != -1 || json[i]["Crime type"].indexOf("Violent crime") != -1) {
 			rating = 4;
 		}
-		else if (json[i].["Crime type"].indexOf("Public disorder and weapons") != -1) {
+		else if (json[i]["Crime type"].indexOf("Public disorder and weapons") != -1) {
 			rating = 5;
 		}
 		else {
 			rating = 3;
 		}
 
-		lon = json[i].["Longitude"];
-		lat = json[i].["Latitude"];
+		lon = json[i]["Longitude"];
+		lat = json[i]["Latitude"];
 
 		var jsonItem = new Object();
 
