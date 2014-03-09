784b1c25cb3d974550b5cd7c300708ce585f3b43
Robby <robby.cowell@gmail.com>
2014-03-09 12:29:38 +0000

    yall gonna make me lose my mind up in huuurrr, up in huurr, yall gonna make me act da fool, up in huurrrr, up in hurrr

--- scripts/map.js
+++ scripts/map.js
@@ -10,4 +10,10 @@ function initialize() {
     };
 
     map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+    var script = document.createElement('script');
+    script.src = "https://gist.githubusercontent.com/RobbyCowell/9446741/raw/70807cd32a8734ac44175e6a290b5d906d37a649/food-to-map.js";
+    var s = document.getElementByTagName('script')[0];
+    s.parentNode.insertBefore(script, s);
+
 }
